fe4ee115114883b7db836ab7d2bfba490d1a0f28
黄楚才 <chucaihuang@kugou.net>
2023-10-24 21:29:49 +0800

    feat: 引入日志系统

--- learnSpring/springAop/src/main/java/com/hcc/spring/aop/aspect/MyLogAspect.java
+++ learnSpring/springAop/src/main/java/com/hcc/spring/aop/aspect/MyLogAspect.java
@@ -1,9 +1,13 @@
 package com.hcc.spring.aop.aspect;
 
+//import com.sun.org.slf4j.internal.Logger;
+//import com.sun.org.slf4j.internal.LoggerFactory;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
@@ -11,6 +15,8 @@ import java.util.Arrays;
 @Component
 @Aspect
 public class MyLogAspect {
+  private static final Logger logger = LoggerFactory.getLogger(MyLogAspect.class);
+
   @Around("generalPointCut()")
   public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
     long start = System.currentTimeMillis();
@@ -18,10 +24,12 @@ public class MyLogAspect {
     String methodName = joinPoint.getSignature().getName();
     Object[] args = joinPoint.getArgs();
     String message = String.format("%s执行了%s方法，参数是%s", packName, methodName, Arrays.toString(args));
-    System.out.println(message);
+//    System.out.println(message);
+    logger.info(message);
     Object obj = joinPoint.proceed();
     long end = System.currentTimeMillis();
-    System.out.println("执行时间" + (end - start) + "ms");
+//    System.out.println("执行时间" + (end - start) + "ms");
+    logger.debug("执行时间" + (end - start) + "ms");
     return obj;
   }
 

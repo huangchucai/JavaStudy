d5a22279f04f39a4a646bee20d2ae3ac9e0eeeb0
chucaihuang <chucaihuang@kugou.net>
2023-11-13 22:23:57 +0800

    feat: 初始化函数式接口的实例

--- java8Learn/java8Learn/src/main/java/com/hcc/InternalFunctionInterfaceDemo.java
+++ java8Learn/java8Learn/src/main/java/com/hcc/InternalFunctionInterfaceDemo.java
@@ -1,14 +1,18 @@
 package com.hcc;
 
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 /**
  * Predicate
- * Consumer
  * Function
+ * Consumer
  * Supplier
  */
 public class InternalFunctionInterfaceDemo {
     public static void main(String[] args) {
+        /**
+         * 1. Predicate
+         */
     }
 }
